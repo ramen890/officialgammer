a731ffb5c36cd4c0c5bd18275ce8489aee65d39f
ramen890 <68506086+ramen890@users.noreply.github.com>
2020-07-19 19:49:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assam Tube</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+	 <nav>
+        <ul class="left">
+
+            <li><a href="#"><i class="fa fa-home"></i></a></li>
+            
+            <li><a href="#">Admit</a></li>
+            <li><a href="#">Results</a></li>
+            <li><a href="https://onlinemarketplace102.blogspot.com/">Blog</a></li>
+            <li><a href="#">About</a></li>
+            
+        </ul>
+	<ul class="right">
+
+            <li><a href="https://www.facebook.com/ASSAM-Tube-107436591001120"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/bhagaban_chutia"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="officialgammer890@gmail.com"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCiX6NmMrTCPpREDYZ3k7Q8A?view_as=subscriber"><i
+                        class="fa fa-youtube"></i></a></li>
+            <li><a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></a></li>
+
+        </ul>
+    </nav>
+
+        <div class="article"><div class="notification">
+         	<h1>Notification</h1>
+         	<div class="notification-box">
+         		<h2><a href="#">Admission</a></h2><br><br>
+         		<h2><a href="#">Resistration</a></h2><br><br>
+         		<h2><a href="#">Festivel</a></h2><br><br>
+         		<h2><a href="#">Sports</a></h2><br><br>
+         	</div>
+         </div>
+
+         <div class="slideshow-container">
+         	<div class="slider">
+         		<img src="biyam.jpg">
+         	</div>
+         	<div class="slider">
+         		<img src="biyam.jpg">
+         	</div>
+         	<div class="slider">
+         		<img src="biyam.jpg">
+         	</div>
+         	<!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+         </div>
+         <div class="article"></div>
+         <h1>Boruah Block High School :</h1>
+         <b>BORUAH BLOCK HS
+BORUAH BLOCK HS</b> was established in 1994 and it is managed by the Department of Education. It is located in Rural area. It is located in DHEMAJI block of DHEMAJI district of Assam. The school consists of Grades from 9 to 10. The school is Co-educational and it doesn't have an attached pre-primary section. The school is Not Applicable in nature and is not using school building as a shift-school. Assamese is the medium of instructions in this school. This school is approachable by all weather road. In this school academic session starts in April.
+         The school has Government building. It has got 0 classrooms for instructional purposes. All the classrooms are in good condition. It has 2 other rooms for non-teaching activities. The school has a separate room for Head master/Teacher. The school has No Boundary Wall boundary wall. The school has have electric connection. The source of Drinking Water in the school is Hand Pumps and it is functional. The school has 1 boys toilet and it is functional. and 1 girls toilet and it is functional. The school has no playground. The school has a library and has 121 books in its library. The school does not need ramp for disabled children to access classrooms.The school has no computers for teaching and learning purposes The school is not having a computer aided learning lab. The school is Not Applicable providing mid-day meal.
+
+         <div class="contact">
+         	<a><h2><i class="fa fa-phone" aria-hidden="true"></i> Contact</h2></a>
+         	<p><b>Prabitra Duwarah :-</p>
+         	<p><b>Jugananda Lalung :-</p>
+         		<p><b>Rupeswar Neog :-</p>
+         </div>
+     	</article>
+
+        
+     </div>
+     <div class="footer">
+     <footer><i class="fa fa-copyright" aria-hidden="true">Boruah Block High School</i></footer>
+     </div>
+
+
+</body>
+</html>
